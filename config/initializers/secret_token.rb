4aa13109a08dd024e37e461607c48fc4c0aa12c9
Juan Carlos Montemayor Elosua <j.mont@me.com>
2013-04-10 13:54:24 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lead::Application.config.secret_token = 'ba904f6c312e9277058b373f4f03922ebf9d334fe80b2c63091aab9ac5df90122f2d95f94db459d9d8e192b8cafb5aafb46968d62008c27bde37f35fff07560f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lead::Application.config.session_store :cookie_store, key: '_lead_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lead::Application.config.session_store :active_record_store
